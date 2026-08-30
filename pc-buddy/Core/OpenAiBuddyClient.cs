d7e49e44135ec54a430f1a3e3c09e0fe7834a950
Herbertofury <stealth.sanchez@hotmail.com>
2026-08-30 15:31:12 -0600

    PCX-029: normalize direct client namespace imports

--- pc-buddy/Core/OpenAiBuddyClient.cs
+++ pc-buddy/Core/OpenAiBuddyClient.cs
@@ -1,8 +1,12 @@
+using System;
+using System.Collections.Generic;
 using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
 using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace PCBuddy.Core;
 
