13060b760d9b95db2cafb34957ef24ab219018ac
Herbertofury <stealth.sanchez@hotmail.com>
2026-08-30 15:28:52 -0600

    PCX-029: add packaged app self-test entrypoint

--- pc-buddy/App.xaml.cs
+++ pc-buddy/App.xaml.cs
@@ -1,12 +1,22 @@
+using PCBuddy.Core;
 using System.Windows;
 
 namespace PCBuddy;
 
 public partial class App : Application
 {
-    protected override void OnStartup(StartupEventArgs e)
+    protected override async void OnStartup(StartupEventArgs e)
     {
         base.OnStartup(e);
+        if (e.Args.Any(arg => string.Equals(arg, "--self-test", StringComparison.OrdinalIgnoreCase)))
+        {
+            ShutdownMode = ShutdownMode.OnExplicitShutdown;
+            var exitCode = await SelfTest.RunAsync();
+            Environment.ExitCode = exitCode;
+            Shutdown(exitCode);
+            return;
+        }
+
         var window = new MainWindow();
         MainWindow = window;
         window.Show();
