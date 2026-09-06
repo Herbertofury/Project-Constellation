a717ecf1acb7377f7896dbf6116a7d7f17be8e95
Herbertofury <stealth.sanchez@hotmail.com>
2026-09-06 13:34:00 -0600

    Wire closed-chat watch vault bridge

--- extension/background-entry.js
+++ extension/background-entry.js
@@ -4,4 +4,5 @@ import './src/closed-chat-watch-core.js';
 import './background.js';
 import './src/notification-repair.js';
 import './src/closed-chat-watch.js';
+import './src/closed-chat-vault-bridge.js';
 import './src/command-center-actions.js';
