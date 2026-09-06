976fc3d3383668b92b57f4b5013816a0fef78617
Herbertofury <stealth.sanchez@hotmail.com>
2026-09-06 12:14:19 -0600

    Wire Command Center quick actions into background worker

--- extension/background-entry.js
+++ extension/background-entry.js
@@ -1,2 +1,5 @@
+import './src/chat-vault-core.js';
+import './src/command-center-action-core.js';
 import './background.js';
 import './src/notification-repair.js';
+import './src/command-center-actions.js';
